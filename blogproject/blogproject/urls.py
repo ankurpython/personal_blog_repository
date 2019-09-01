8d64f7286aa3be9fdf8344b110ed2d455218ca0a
suman shekhar <shekhar.shs@gmail.com>
2019-09-01 19:18:01 +0530

    Personal Blog from scratch to completion developed

---
+++ blogproject/blogproject/urls.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.urls import path,re_path,include
+from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.post_list_view),
+    path('add_post', views.add_post),
+    path('add_profile', views.add_profile),
+    path('like/', views.like_post,name='like_post'),
+    path('update_post/<int:id>', views.update_post,name='update_post'),
+    path('post_delete/<int:id>', views.post_delete,name='post_delete'),
+    path('profile', views.profile_view),
+    path('index', views.index),
+    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list_view,name='post_list_by_tag_name'),
+    path('contact/', views.contact_view),
+    path('about/', views.about_view),
+    #re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail_view, name='detail_view'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail_view,name='detail_view'),
+    path('<int:id>/share/', views.mail_send_view),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('oauth/', include('social_django.urls',namespace='social')),
+    path('logout/',views.logout_view),
+    path('change_password/',views.change_password),
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='blog/password_reset.html'),name='password_reset'),
+    path('password_reset_comlete/',auth_views.PasswordResetCompleteView.as_view(template_name='blog/password_reset_comlete.html'),name='password_reset_comlete'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='blog/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password_reset/done',auth_views.PasswordResetDoneView.as_view(template_name='blog/password_reset_done.html'),name='password_reset_done'),
+    path('signup/',views.signup_view),
+
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
